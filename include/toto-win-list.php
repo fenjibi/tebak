5dbc36adb3e4d44ae68a7959e7f66b1c7cdb805e
william fenji <shinichi.fenji@gmail.com>
2017-05-28 00:46:42 +0700

    start user's winner list
    - make all toto winner list for user.
    - rename toto winner application on right side.

---
+++ include/toto-win-list.php
@@ -0,0 +1,41 @@
+<div id="main_content">
+	<table id="togel-winlist">
+		<thead>
+			<tr>
+				<th>Periode</th>
+				<th>Username</th>
+				<th>Number</th>
+			</tr>
+		</thead>
+		<tbody>
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/bet.php";
+$winner_list = $bet->toto_winner_list();
+foreach ($winner_list as $wlist){
+	echo "<tr towinid='".$wlist['togel_win_id']."'><td>".$wlist['periode']."</td><td>".$wlist['username']."</td><td class='tebakan'>".$wlist['number']."</td></tr>";
+}
+?>
+		</tbody>
+	</table>
+</div>
+<style>
+#togel-winlist {
+	width: 100%;
+}
+#togel-winlist > thead > tr > th {
+	background-color: #c6ff00;
+    color: #000;
+	padding: 5px;
+}
+#togel-winlist > tbody > tr > td {
+	background-color: #fff;
+    color: #000;
+	font-size: 13px;
+    padding: 5px;
+	text-align: center;
+}
+#togel-winlist .tebakan {
+	font-weight: bold;
+    color: #ff0707;
+}
+</style>
